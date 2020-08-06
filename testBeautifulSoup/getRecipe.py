6e45c263d1212784ffd72d6b12492a5baa756fa4
jajaja12345 <vie.estbeats@gmail.com>
2020-08-07 01:17:50 +0900

    余分なコメントを消した,テキスト取得を関数化した

--- testBeautifulSoup/getRecipe.py
+++ testBeautifulSoup/getRecipe.py
@@ -11,29 +11,36 @@ def stripBlank(text):
     rValue = "\n".join(line for line in lines if line)
     return rValue
 
+
+def getText(a, csSelec):
+    try:
+        # ingC = a.select_one(".ingredient_category").text
+        te = stripBlank(a.select_one(csSelec).text)
+
+    except AttributeError as e:
+        # print("Attributeerror occurs (in category)")
+        if csSelec == ".ingredient_category":
+            return None
+        return ""
+    except:
+        # print("some error occurs(in category)")
+        if csSelec == ".ingredient_category":
+            return None
+        return ""
+    else:
+        return te
+
+
 # Webページを取得して解析する
-
-
 # print("取得元のurlを入力")
-url =
-# url = input()
+# url =
+url = input()
 # print(url)
 html = requests.get(url)
 soup = BeautifulSoup(html.content, "html.parser")
 
 print("title:" + stripBlank(soup.find(
     id="recipe-title").select_one(".recipe-title.fn.clearfix").get_text()))
-# print("recipe title:" + title)
-
-"""空行を外す処理
-lines = []
-for line in title.splitlines():
-    lines.append(line.strip())
-# print(len(lines))
-Ltext = "\n".join(line for line in lines if line)
-print(Ltext)
-"""
-
 
 ing = soup.find(id="ingredients")
 serv = ing.select_one(".servings_for.yield").text
@@ -42,60 +49,18 @@ print("servings_for yield:" + stripBlank(serv))
 
 ingS = ""
 ingRow = soup.find(id="ingredients_list").select(".ingredient_row")
-# print(len(ingRow))
+
 for a in ingRow:
-    try:
-        # ingC = a.select_one(".ingredient_category").text
-        ingS += stripBlank(a.select_one(".ingredient_category").text) + '\n'
-
-    except AttributeError as e:
-        # print("Attributeerror occurs (in category)")
-        #
-        pass
-    except:
-        # print("some error occurs(in category)")
-        pass
-    else:
-        pass
-
-    try:
-        ingS += stripBlank(a.select_one(".name").text) + ":"
-    except AttributeError as e:
-        # print("Attributeerror occurs (in name)")
-        pass
-        # continue
-    except:
-        # print("some error occurs(in name)")
-        # continue
-        pass
-    else:
-        # print(name)
-        pass
-
-    try:
-        ingS += stripBlank(a.select_one(".ingredient_quantity.amount").text) + '\n'
-    except AttributeError as e:
-        # print("Attributeerror occurs (in quantity)")
-        ingS += '\n'
-        pass
-        # continue
-    except:
-        # print("some error occurs(in quantity)")
-        # continue
-        pass
-    else:
-        # print(qAmount)
-        pass
+    temp = getText(a, ".ingredient_category")
+    if temp != None:
+        ingS += temp + '\n'
+    ingS += getText(a, ".name") + ":"
+    ingS += getText(a, ".ingredient_quantity.amount") + '\n'
 print("材料:")
 print(ingS)
 
 print("step:")
 ingStep = soup.find(id="steps").select(".step")
-# ingStep = soup.find(id = "")
-# print(len(ingStep)) -> 5
+
 for a in ingStep:
     print(stripBlank(a.select_one(".instruction").select_one(".step_text").text))
-
-
-# HTML全体を表示する
-# print(soup)
